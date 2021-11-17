35e3db7e929cdc58e7d97e006743438a947ddbc9
jhonattansousa <jhonatann95@hotmail.com>
2021-11-17 20:59:54 -0300

    adicionar estelização ao body

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ let food = {
 }
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "pink";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "purple";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function drawfood() {
-    context.fillStyle = "red";
+    context.fillStyle = "blue";
     context.fillRect(food.x, food.y, box, box);
 }
 
@@ -36,15 +36,21 @@ function update (event){
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
     if(event.keyCode == 40 && direction != "up") direction = "down";
-
-
 }
 
 function iniciarJogo(){
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake [i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Gamer Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -64,8 +70,6 @@ function iniciarJogo(){
     else{food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-
-
 
     let newHead = {
         x: snakeX,
